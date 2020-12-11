dd3491b2d5e04d2164b3398d514a53c9c85d4ad9
Constantin VdV <vandevelde-c@hotmail.fr>
2020-12-11 10:43:01 +0100

    [ADD] Topic finder, which allows to find topics according to the top words per topic

---
+++ amendements_analysis/domain/topic_finder.py
@@ -0,0 +1,165 @@
+import amendements_analysis.settings.base as stg
+import numpy as np
+import pandas as pd
+import os 
+import re
+import spacy
+import unicodedata, unidecode
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+os.system('python3 -m spacy download fr_core_news_md')
+import fr_core_news_md
+
+class TextCleaner():
+    """
+    Get amendments df and perform pre processing compatible with Topics words finder 
+    This includes :
+        - remove accent
+        - lemmatize
+        - specific regex flag for specific terms
+
+    Attributes
+    ----------
+    df: pandas.DataFrame
+    ----------
+    Return:
+    df_cleaned: pandas.DataFrame 
+    """
+    def __init__(self, flag_dict):
+        """Class initilization
+        Parameters
+        ----------
+        flag_dict: dict of regex to replace by flag
+
+        """
+        self.flag_dict = flag_dict
+
+    def fit(self, df): 
+        return self
+    
+    def transform(self, df):
+        df_cleaned = df.copy()
+        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(lambda x: str(x))
+        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(self.lemmatizer)
+        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(self.lowercase)
+        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(self.flag_text, args=(self.flag_dict,))
+        return df_cleaned
+    
+    @staticmethod
+    def lowercase(text):
+        return text.lower()
+
+    @staticmethod
+    def remove_accents(text, method='unicodedata'):
+
+        if method == 'unidecode':
+            return unidecode.unidecode(text)
+        elif method == 'unicodedata':
+            utf8_str = unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode("utf-8")
+            return utf8_str
+        else:
+            raise ValueError("Possible values for method are 'unicodedata' or 'unidecode'")    
+
+    @staticmethod
+    def flag_text(text, flag_dict):
+        for regex, flag in flag_dict.items():
+            text = re.sub(regex, flag, str(text))
+
+        return text   
+
+    @staticmethod  
+    def lemmatizer(text):
+      nlp = fr_core_news_md.load(disable=['ner', 'parser'])
+      nlp.add_pipe(nlp.create_pipe('sentencizer'))
+      doc = nlp(text)
+      text_lemm = [token.lemma_ for token in doc]
+      text_lemm = ' '.join(text_lemm)
+      return str(text_lemm)
+
+
+class TopicWordsFinder(): 
+    """
+    Get amendements df with labels from model and perform clustering words recognition
+
+    Attributes
+    ----------
+    df: pandas.DataFrame
+    ----------
+    Return:
+    
+    words_per_topic: Array of top words with tf-idf score per topic 
+    topic_size: pandas.DataFrame 
+    """
+    def __init__(self, df):
+        """Class initilization
+        Parameters
+        ----------
+        df: cleaned and lemmatized dataframe with topic already assigned from previous clusterization
+        """
+
+        self.df = df
+
+    @property
+    def words_per_topic(self):
+        df_topic = self.get_df_topic(self.df)
+        count_vectorizer = self.get_countvectorizer(df_topic[stg.AMENDEMENT])
+        c_tf_idf = self.get_custom_tfidf(df_topic[stg.AMENDEMENT], len(self.df), count_vectorizer)
+        top_n_words = self.extract_top_n_words_per_topic(c_tf_idf, count_vectorizer, df_topic, n=20)
+        topic_sizes = self.extract_topic_sizes(self.df)
+        return top_n_words, topic_sizes
+
+    def get_df_topic(self, df):
+        df_topic = df.copy()
+        df_topic[stg.AMENDEMENT] = df_topic[stg.AMENDEMENT].apply(lambda x: str(x))
+        df_topic = df_topic.groupby([stg.TOPIC]).agg({stg.AMENDEMENT: ' '.join}).reset_index()
+        return df_topic
+    
+    def get_countvectorizer(self, doc):
+        count_vectorizer = CountVectorizer(**stg.PARAMS_CV)
+        count_vectorizer.fit(doc)
+        return count_vectorizer
+    
+    def get_custom_tfidf (self, doc, m, count_vectorizer):
+        topics = count_vectorizer.transform(doc).toarray()
+        words = topics.sum(axis=1)
+        tf = np.divide(topics.T, words)
+        sum_t = topics.sum(axis=0)
+        idf = np.log(np.divide(1+m, 1+sum_t)+1).reshape(-1, 1)
+        tf_idf = np.multiply(tf, idf)
+        return tf_idf
+    
+    def extract_top_n_words_per_topic(self, tf_idf, count_vectorizer, df_topic, n):
+        words = count_vectorizer.get_feature_names()
+        labels = list(df_topic[stg.TOPIC])
+        tf_idf_transposed = tf_idf.T
+        indices = tf_idf_transposed.argsort()[:, -n:]
+        top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+        return top_n_words
+    
+    def extract_topic_sizes(self,df):
+        topic_sizes = (df.groupby([stg.TOPIC], sort=True)
+                         [stg.AMENDEMENT]
+                         .count()
+                         .reset_index()
+                         .rename({stg.AMENDEMENT: 'Size'}, axis='columns')
+                         .sort_values('Size', ascending=False))
+        return topic_sizes
+
+if __name__ == "__main__":
+    df = pd.read_csv(os.path.join(stg.DATA_DIR, "amendements_sentence.csv"))
+    df = df.iloc[::25000]
+    cleaner = TextCleaner(flag_dict=stg.FLAG_DICT)
+    df_cleaned = cleaner.transform(df)
+    print(df_cleaned[stg.AMENDEMENT].head())
+    df_f = df_cleaned.copy()
+    df_f['Topic'] = 0
+    import random
+    for i in range(df_f.shape[0]):
+        df_f['Topic'].iloc[i] = random.randrange(0,3,1)
+    word_finder = TopicWordsFinder(df_f)
+    top_n_words, topic_sizes = word_finder.words_per_topic
+    for i in range(0,(len(topic_sizes))):
+        print(i, top_n_words[i][:10])
+    print(topic_sizes.head(25))
+
--- amendements_analysis/settings/base.py
+++ amendements_analysis/settings/base.py
@@ -1,4 +1,9 @@
 import os
+from nltk.corpus import stopwords
+import nltk
+
+nltk.download('stopwords')
+STOPWORDS_LIST = stopwords.words("french")
 
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
@@ -20,7 +25,6 @@ SOURCE_COLUMNS = [
     "texteLegislatifRef",
     "corps",
 ]
-test = "bla"
 
 COLUMNS = [
     "uid",
@@ -36,21 +40,36 @@ COLUMNS = [
 ]
 
 AMENDEMENT = "expose_sommaire"
-FLAG_DICT = {
-    r'/^(?!(?:1975)$)\d+/' : 'flag_numero',
-    r'\bANCT\b' : 'flag_territoire',
-    r'\b1975\b' : 'flag_retraite',
-    r'\b[A-Za-z]\b': ''
+TOPIC = 'Topic'
+
+PARAMS_CV = { 
+    'strip_accents': 'unicode',
+    'stop_words': STOPWORDS_LIST,
+    'max_df': 0.9,
+    'min_df': 0.01,
+    'ngram_range': (1,2)
 }
+
+FLAG_DICT = {
+    r'\b1975\b' : 'flag_retraite',
+    r'\d+' : 'flag_numero',
+    r'\bANCT\b' : 'flag_territoire',
+    r'\b[A-Za-z]\b': '',
+    r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})':'flag_url'
+}
+
 TOPICS_DICT = {
-    '0': 'Administratif (vote, scrutin, marchés publics)',
-    '1': 'Retrait & contestation de loi / decret etc',
-    '2': 'Immobilier & urbanisme',
-    '3': 'Naissances & procreation',
-    '4': 'Justice, sécurité, pénal',
-    '5': 'Politique echelle commune & regions',
-    '6': 'Travail,Entreprises',
-    '7': 'Fiscalité des particuliers',
-    '8': 'Santé, Handicap',
-    '9': 'Ecologie, Traitement dechet'
+    '0': 'Ecologie, Rechauffement climatique',
+    '1': 'Justice, Droit, Sécurité, Pénal',
+    '2': 'Retraites',
+    '3': 'Naissances, Procreation & condition de la Jeunesse',
+    '4': 'Medecine & Santé',
+    '5': 'Fiscalité / Aides des Entreprises & Particuliers' ,
+    '6': 'Politique Cohésion Territoriale (National)',
+    '7': 'Immobilier & Urbanisme',
+    '8': 'Alimentaire, Elevage, Agriculture & traitement des Déchets',
+    '9': 'Rectification / contestations de lois (vote, scrutin)',
+    '10': 'Service Publique (éducations, transports publics ...)',
+    '11': 'Financements des Secteurs',
+    '12': 'Gestion Budgétaire'
 }
