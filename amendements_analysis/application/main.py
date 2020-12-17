403c1967c7940b9419e6472d1cb31689582d6119
Thomas Barnouin <barnouin.thomas@gmail.com>
2020-12-17 21:58:45 +0100

    [ADD] UMAP model to streamlit app

--- amendements_analysis/application/main.py
+++ amendements_analysis/application/main.py
@@ -1,45 +1,49 @@
 import amendements_analysis.settings.base as stg
 from amendements_analysis.infrastructure.building_dataset import DatasetBuilder
-from amendements_analysis.infrastructure.sentence_encoding import DatasetCleaner
+from amendements_analysis.infrastructure.sentence_encoding import DatasetCleaner, TextEncoder
 from amendements_analysis.domain.lda_model import LatentDirichletAllocationModel
-import pyLDAvis.sklearn
-import pyLDAvis
-import streamlit as st
+from amendements_analysis.domain.topic_predicter import topic_predicter
+from sklearn_extra.cluster import KMedoids
 import matplotlib.pyplot as plt
-from streamlit.components.v1 import iframe
+from streamlit.components.v1 import html
+import umap.umap_ as umap
+import os
 import pickle
+import numpy as np
 
 
-# Selection of the method of classification
-method_of_classification = st.sidebar.selectbox("Select classifier", ("KNN", 'LDA'))
+#sentence_embeddings = np.load(os.path.join(stg.DATA_DIR, "FinedTunedBert_fullDF.npy"))
 
-# Parameters settings
-if method_of_classification == "LDA":
-    # Loading of the LDA model
-    vis = pickle.load(open("../../data/viz.html", "rb"))
-    params = {}
-    topic_number = st.sidebar.slider("topic", 1, 10)
-    params['topic'] = topic_number
-    lam = st.sidebar.slider("lambda", 0., 1.)
-    params["lambda"] = lam
-else:
-    params = {}
+#umap_model = umap.UMAP(n_neighbors=15, 
+#                        n_components=15, 
+#                        metric='cosine')
+#pickle.dump(umap_model_fit, open(os.path.join(stg.DATA_DIR,'umap_model_fitted.sav'), 'wb'))
 
+a = ["""Cet amendement du groupe Socialistes et apparentés vise à fermer complètement la porte à la minoration des variables d'ajustement , à hauteur de 50 millions d’euros, de deux concours financiers de l’État aux collectivités territoriales.
 
-def get_classification(method_of_classification):
-    if method_of_classification == "LDA":
-        vis.topic_info["Lambda"] = round(1 - vis.topic_info["Freq"]/vis.topic_info["Total"], 1)
-        st.write(vis.topic_info[(vis.topic_info.Category == 'Topic'+str(params['topic'])) & (vis.topic_info.Lambda < params['lambda'] + 0.1)]\
-        .filter(["Term", "Freq"]))
-    else:
-        #fig = plt.figure()
-        #plt.scatter(knn["x"], knn["y"], c=knn["topics"], cmap="viridis")
-        #plt.colorbar()
-        #st.pyplot(fig)
-        pass
+Pour rappel, l’article 22  dans sa rédaction initiale minore de 50 millions deux concours financiers de l'Etat aux collectivités territoriales (appelés variables d’ajustement), à savoir :
 
+Dotation de compensation de la réforme de la taxe professionnelle (DCRTP) :
 
-get_classification(method_of_classification)
+- Part régionale : -7,5 millions d'euros
+- Part départementale : -5 millions d'euros
+
+Dotation de transfert des compensations d’exonération de taxe d’habitation (DTCE) :
+
+- Part régionale : -17,5 millions d'euros
+- Part départementale : -20 millions d'euros
+
+Ces minorations avaient atteint 120 millions d’euros en 2020, 159 millions d’euros en 2019 et 293 millions d’euros en 2018.
+
+La DCRTP et la DTCE ont été créées lors de la réforme de la taxe professionnelle et de la fiscalité locale, intervenue en 2010, pour compenser les collectivités perdantes dans le cadre de cette réforme, en vue d’en assurer la neutralité financière. Ces dotations, qui se substituaient à des ressources fiscales dynamiques, avaient donc vocation à être figées sur le montant initialement fixé.
+
+L’introduction de la DCRTP et de la DTCE au sein des variables d’ajustement est donc une mesure injuste et difficilement acceptable pour les régions et départements."""]
 
 
+sentence = TextEncoder(a,
+finetuned_bert = False, batch_size=1).sentence_embeddings
 
+umap_model_fit = pickle.load(open(os.path.join(stg.DATA_DIR,'umap_model_fitted.sav'), 'rb'))
+cluster_model_fit = pickle.load(open(os.path.join(stg.DATA_DIR, stg.CLUSTER_MODEL_FIT_FILENAME),"rb"))
+prediction = topic_predicter(sentence, umap_model_fit, cluster_model_fit).predicted_topic
+print('prediction is : ', prediction)
---
+++ amendements_analysis/application/streamlit_app.py
@@ -0,0 +1,36 @@
+import amendements_analysis.settings.base as stg
+from amendements_analysis.infrastructure.building_dataset import DatasetBuilder
+from amendements_analysis.infrastructure.sentence_encoding import DatasetCleaner, TextEncoder
+from amendements_analysis.domain.lda_model import LatentDirichletAllocationModel
+from amendements_analysis.domain.topic_predicter import topic_predicter
+import streamlit as st
+import matplotlib.pyplot as plt
+from streamlit.components.v1 import html
+import umap.umap_ as umap
+import pickle
+import os
+import numpy as np
+
+# Selection of the method of classification
+method_of_classification = st.sidebar.selectbox("Sélectionner la méthode de classification", ("UMAP", "LDA"))
+
+# Parameters settings
+if method_of_classification == "UMAP":
+    # Loading the UMAP model
+    cluster_model_fit = pickle.load(open(os.path.join(stg.DATA_DIR, stg.CLUSTER_MODEL_FIT_FILENAME),"rb"))
+    umap_model_fit = pickle.load(open(os.path.join(stg.DATA_DIR,'umap_model_fitted.sav'), 'rb'))
+    amendement = st.text_input("Amendement à classifier")
+    submit = st.button('Prediction')
+    if submit:
+        amendement = np.array([amendement])
+        amendement = amendement.tolist()
+        st.write(amendement)
+        sentence_embeddings = TextEncoder(amendement, finetuned_bert = True, batch_size=1).sentence_embeddings
+        prediction = topic_predicter(amendement, umap_model_fit, cluster_model_fit).predicted_topic
+        st.write('Le sujet identifié est : ', prediction)
+elif method_of_classification == "LDA":
+    # Loading of the LDA model
+    with open(os.path.join(stg.DATA_DIR, "pyLDAvis.html"), 'r') as f:
+        html_string = f.read()
+        html(html_string, width = 1300, height = 800)
+
