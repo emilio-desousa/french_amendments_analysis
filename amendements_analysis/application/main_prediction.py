0aec394b0b2aaf9e253b20a0d4fca2b7270d0b46
Constantin VdV <vandevelde-c@hotmail.fr>
2020-12-18 18:48:52 +0100

    [ADD] Poetry script for main_model.py train & predict

--- amendements_analysis/application/main_prediction.py
+++ amendements_analysis/application/main_prediction.py
@@ -5,14 +5,25 @@ import pickle
 import joblib
 import os
 
-cluster_model = pickle.load(open(os.path.join(stg.DATA_DIR, stg.CLUSTER_MODEL_FIT_FILENAME_EXT), "rb"))
-umap_model = joblib.load(os.path.join(stg.DATA_DIR, stg.UMAP_MODEL_FIT_FILENAME_EXT))
+def main_model_predict():
 
-amendement_list = []
-amendement = input("Enter one law text: ")
-amendement_list.append(amendement)
+    amendement_list = []
+    amendement = input("Enter one law text: ")
+    amendement_list.append(amendement)
+    encoder_predict = TextEncoder(amendement_list, finetuned_bert = True, batch_size=1)
+    sentence_embedding = encoder_predict.sentence_embeddings
+    try:
+        prediction_number, _ = topic_predicter(sentence_embedding, umap_model, cluster).predicted_topic
+        print('prediction is : ', prediction_number)
+    except NameError:
+        if os.path.isfile(os.path.join(stg.DATA_DIR, stg.CLUSTER_MODEL_FIT_FILENAME_EXT)) and\
+           os.path.isfile(os.path.join(stg.DATA_DIR, stg.UMAP_MODEL_FIT_FILENAME_EXT)):
+            cluster_model_fit = pickle.load(open(os.path.join(stg.DATA_DIR, stg.CLUSTER_MODEL_FIT_FILENAME_EXT), "rb"))
+            umap_model_fit = joblib.load(os.path.join(stg.DATA_DIR, stg.UMAP_MODEL_FIT_FILENAME_EXT))
+            prediction_number, _ = topic_predicter(sentence_embedding, umap_model_fit, cluster_model_fit).predicted_topic
+            print('prediction (from saved model) is : ', prediction_number)
+        else:
+            print('No model UMAP & CLUSTERS are available, please generate one')
 
-encoder_predict = TextEncoder(amendement_list, finetuned_bert = True, batch_size=1)
-sentence_embedding = encoder_predict.sentence_embeddings
-prediction_number, _ = topic_predicter(sentence_embedding, umap_model, cluster_model).predicted_topic
-print('prediction is : ', prediction_number)
+if __name__ == "__main__" :
+    main_model_predict()
