e84128b75ec56e0fac194ba270c65aa967b3d015
de sousa emilio <palimpseste.archi@gmail.com>
2020-12-18 11:41:53 +0100

    [FIX] speed up lemm

--- amendements_analysis/domain/topic_finder.py
+++ amendements_analysis/domain/topic_finder.py
@@ -1,4 +1,5 @@
 import amendements_analysis.settings.base as stg
+from amendements_analysis.infrastructure.building_dataset import DatasetBuilder
 import numpy as np
 import pandas as pd
 import os
@@ -6,7 +7,6 @@ import re
 import spacy
 import unicodedata, unidecode
 from sklearn.feature_extraction.text import CountVectorizer
-
 import fr_core_news_md
 
 
@@ -38,14 +38,15 @@ class TextCleaner:
     def fit(self, df):
         return self
 
+    ##
+    ## [TODO] Create Batch for lem
+    ##
     def transform(self, df):
         df_cleaned = df.copy()
-        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(lambda x: str(x))
-        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(self.lemmatizer)
-        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(self.lowercase)
-        df_cleaned[stg.AMENDEMENT] = df_cleaned[stg.AMENDEMENT].apply(
-            self.flag_text, args=(self.flag_dict,)
-        )
+        df_cleaned = df_cleaned.apply(lambda x: str(x))
+        df_cleaned = self.lemmatizer(df_cleaned)
+        # df_cleaned = df_cleaned.apply(self.lowercase)
+        df_cleaned = df_cleaned.apply(self.flag_text, args=(self.flag_dict,))
         return df_cleaned
 
     @staticmethod
@@ -77,13 +78,18 @@ class TextCleaner:
         return text
 
     @staticmethod
-    def lemmatizer(text):
+    def lemmatizer(df):
+        df_tmp = []
         nlp = fr_core_news_md.load(disable=["ner", "parser"])
         nlp.add_pipe(nlp.create_pipe("sentencizer"))
-        doc = nlp(text)
-        text_lemm = [token.lemma_ for token in doc]
-        text_lemm = " ".join(text_lemm)
-        return str(text_lemm)
+        for doc in nlp.pipe(list(df), batch_size=2000):
+            texts = [ent.lemma_ for ent in doc]
+            tmp = " ".join(texts)
+            df_tmp.append(tmp)
+        # doc = nlp.pipe(text)
+        # text_lemm = [token.lemma_ for token in doc]
+        # text_lemm = " ".join(text_lemm)
+        return pd.DataFrame(df_tmp)[0]
 
 
 class TopicWordsFinder:
@@ -167,10 +173,28 @@ class TopicWordsFinder:
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(os.path.join(stg.DATA_DIR, "amendements_sentence.csv"))
-    df = df.iloc[::25000]
+    import time
+
+    df = DatasetBuilder(get_only_amendments=True).data
+    print(df.shape)
+    # df = pd.read_csv(os.path.join(stg.INTERIM_DIR, "amendements_sentence.csv"))
+    df = df.iloc[:100]
     cleaner = TextCleaner(flag_dict=stg.FLAG_DICT)
     df_cleaned = cleaner.transform(df)
+    df_tmp = []
+    index = 0
+
+    # nlp.add_pipe(nlp.create_pipe("sentencizer"))
+    # print(doc)
+    # nlp = fr_core_news_md.load(disable)
+    # nlp.add_pipe(nlp.create_pipe("sentencizer"))
+    # doc = nlp.pipe(list(df))
+    # data_cleaned = []
+    # cleaner.lemmatizer()
+    # for batch in df_cleaned:
+    # print(cleaner.lemmatizer(list(batch)))
+    # data_cleaned.append(cleaner.lemmatizer(list(batch)))
+    """
     print(df_cleaned[stg.AMENDEMENT].head())
     df_f = df_cleaned.copy()
     df_f["Topic"] = 0
@@ -183,3 +207,4 @@ if __name__ == "__main__":
     for i in range(0, (len(topic_sizes))):
         print(i, top_n_words[i][:10])
     print(topic_sizes.head(25))
+"""
